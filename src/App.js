7896f7e2d315c65a59e695395e0c99e5528044ff
Atish Maharjan <arcrt1993@gmail.com>
2020-06-04 15:27:35 +0545

    face detection initialized with Clarifai API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/navigation/navigation';
+import Logo from './components/logo/logo';
+import Rank from './components/rank/rank';
+import FaceRecognition from './components/facerecognition/facerecognition';
+import ImageLinkForm from './components/imagelinkform/imagelinkform';
 import './App.css';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: '7873af76690246098c42a8bbb08b4493'
+});
+
+const particleOptions={
+    particles: {
+     number: {
+       value: 30,
+        density: {
+          enable: true, 
+          value_area: 200
+        }
+     }
+    }
+  }
+
+class App extends React.Component {
+  
+      state = {
+        input: '',
+        imgUrl:''
+      }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit=()=>{
+    this.setState({imgUrl: this.state.input})
+    // console.log(['log', this.state.input, this.state.imgUrl])
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+       this.state.input).then(
+        function(response) {
+          // do something with response
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function(err) {
+          // there was an error
+        }
+    );
+  }
+
+  render(){
+    return (
+      <div className="App">
+              <Particles className='particles'
+                params={particleOptions}  
+              />
+        <link rel="stylesheet" href="https://unpkg.com/tachyons@4/css/tachyons.min.css"/>
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonClick={this.onSubmit}/>
+        <FaceRecognition imageUrl={this.state.imgUrl}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/imagelinkform/imagelinkform.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './imagelinkform.css';
+
+const ImageLinkForm = ({onInputChange, onButtonClick}) => {
+    return (
+       <div className='ma4 mt0'>
+          <p className='f3'>
+            {'This app will detect faces from pictures'}
+          </p>
+            <div className='center'>
+              <div className='center form pa4 br3 shadow-5'>
+                <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange}/>
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple br3' onClick={onButtonClick}>Detect</button>
+              </div>  
+            </div>
+       </div>
+    );
+}
+
+export default ImageLinkForm;

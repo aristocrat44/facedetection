a1fe64e706da8833f320d5b5165895354450cace
Atish Maharjan <arcrt1993@gmail.com>
2020-06-05 10:33:58 +0545

    added placeholder in inputbox

--- src/components/imagelinkform/imagelinkform.js
+++ src/components/imagelinkform/imagelinkform.js
@@ -9,7 +9,7 @@ const ImageLinkForm = ({onInputChange, onButtonClick}) => {
           </p>
             <div className='center'>
               <div className='center form pa4 br3 shadow-5'>
-                <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange}/>
+                <input type='text' placeholder='enter valid image url' className='f4 pa2 w-70 center' onChange={onInputChange}/>
                 <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple br3' onClick={onButtonClick}>Detect</button>
               </div>  
             </div>

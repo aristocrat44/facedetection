255b9fd32c6693257ff0da75824120944986c6f6
Atish Maharjan <arcrt1993@gmail.com>
2020-06-05 10:35:57 +0545

    Updates

---
+++ precache-manifest.d9d612b4cf131edf37201d2fdd32e725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3498553d2e312cdf62bde0e534423c",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "5d0f129e75c8fe5e105d",
+    "url": "/facedetection/static/css/main.3af4d9bb.chunk.css"
+  },
+  {
+    "revision": "fabdddaab9afed48843a",
+    "url": "/facedetection/static/js/2.5d65feee.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetection/static/js/2.5d65feee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0f129e75c8fe5e105d",
+    "url": "/facedetection/static/js/main.2dca5acb.chunk.js"
+  },
+  {
+    "revision": "1255b356dd9bca1f7ba3",
+    "url": "/facedetection/static/js/runtime-main.4859cbee.js"
+  }
+]);
